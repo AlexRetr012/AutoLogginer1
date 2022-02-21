1a5dd175a4e6b751a342733614ae3dac14be02ea
AlexRetr012 <printhelloworld@mail.ru>
2022-02-21 20:51:12 +0300

    0.0.2(Going Furter)

--- import os.py
+++ import os.py
@@ -1,18 +1,53 @@
 import os
+import getpass
+from tabnanny import check
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+def checkCredsMirea(log, passw):
+    loadBar = "Trying to log in..."
+    statusConnected = False
+    driver = webdriver.Chrome("chromedriver")
+    driver.get("https://login.mirea.ru/login/?next=/oauth2/v1/authorize/%3Fresponse_type%3Dcode%26client_id%3DdnOh7sdtPxfyxzbxcMRLksWlCCE3WsgTfRY6AWKh%26redirect_uri%3Dhttps%253A%252F%252Fonline-edu.mirea.ru%252Flogin%252F%26scope%3Dbasic%2Bstudent")
+    driver.find_element_by_id("id_login").send_keys(log)
+    driver.find_element_by_id("id_password").send_keys(passw)
+    driver.find_element_by_
+
+
+
+
+
+def checkCredsLocal(log, passw):
+    os.system('cls||clear')
+    print('Wait. Checking your credentials...\n')
+    time.sleep(3)
+
+    if(log.find("@edu.mirea.ru")!= -1):
+        print('Mail validation sucessful')
+        time.sleep(3)
+        os.system('cls||clear')
+        checkCredsMirea(log,passw)
+    else:
+        print('Login is incorrect')
+        answer=input('Want to try once again? (Y/n) >> ')
+        if (answer =="Y" or answer == "y" or answer == "д" or answer == "Д"):
+            os.system('cls||clear')
+            task()
+        else:
+            os.system('cls||clear')
+            print("Goodbye!")
+            time.sleep(3)
+
 
 def task():
     os.system('clear' if os.name == 'posix' else 'cls')
+    print('Welcome to AutoLogginer Lite.\nWarning!We don`t collect/sell/store your logins & passwords \nVer 0.0.1(before release)\n')
 
-    print('Welcome to AutoLogginer Lite.\nWarning!We don`t collect/sell/store your logins & passwords \nVer 0.0.1(before release)')
-    print('Enter your MIREA login >> ')
-    
+    userCredL = input('Enter your MIREA login >> ')
+    userCredP = getpass.getpass('Enter your MIREA password >> ')
 
-
-
-
-
-
-
+    checkCredsLocal(userCredL,userCredP)
 
 
 if __name__ == '__main__':
