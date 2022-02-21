c110bc3e2117f374c358b5991fc350cdfc044392
AlexRetr012 <printhelloworld@mail.ru>
2022-02-21 22:15:43 +0300

    0.0.2b

--- import os.py
+++ import os.py
@@ -2,20 +2,50 @@ import os
 import getpass
 from tabnanny import check
 import time
+from datetime import datetime, date, time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+
+def dayOfStudy():
+    if(datetime.today().isoweekday()==1):
+        print("Monday")
+    if(datetime.today().isoweekday()==2):
+        print("Tuesday")
+    if(datetime.today().isoweekday()==3):
+        print("Wednesday")
+    if(datetime.today().isoweekday()==4):
+        print("Thursday")
+    if(datetime.today().isoweekday()==5):
+        print("Friday")
+    if(datetime.today().isoweekday()==6):
+        print("Saturday")
+    if(datetime.today().isoweekday()==7):
+        print("Sunday")   
+
+
+
 
 def checkCredsMirea(log, passw):
     loadBar = "Trying to log in..."
     statusConnected = False
    
     driver = webdriver.Chrome("chromedriver")
-    driver.get("https://online-edu.mirea.ru/")
-    driver.find_element_by_id("login_username").send_keys(log)
-    driver.find_element_by_id("login_password").send_keys(passw)
-    driver.find_element_by_class_name("btn btn-primary btn-block").click()
+    driver.get("https://login.mirea.ru/login/?next=/oauth2/v1/authorize/%3Fresponse_type%3Dcode%26client_id%3DdnOh7sdtPxfyxzbxcMRLksWlCCE3WsgTfRY6AWKh%26redirect_uri%3Dhttps%253A%252F%252Fonline-edu.mirea.ru%252Flogin%252F%26scope%3Dbasic%2Bstudent")
+    driver.find_element_by_id("id_login").send_keys(log)
+    driver.find_element_by_id("id_password").send_keys(passw)
+    if (driver.find_element_by_xpath("//option[text()='Войти']")==True):
+        print("yes")
+    else:
+        print("no")
 
-
+    WebDriverWait(driver=driver, timeout=10).until(lambda x: x.execute_script("return document.readyState === 'complete'"))
+    error_message = "Incorrect username or password."
+    errors = driver.find_elements_by_class_name("flash-error")
+    print(e.text)
+    if any(error_message in e.text for e in errors):
+        print("[!] Login failed")
+    else:
+        print("[+] Login successful")
 
 
 
@@ -44,7 +74,7 @@ def checkCredsLocal(log, passw):
 def task():
     os.system('clear' if os.name == 'posix' else 'cls')
     print('Welcome to AutoLogginer Lite.\nWarning!We don`t collect/sell/store your logins & passwords \nVer 0.0.1(before release)\n')
-
+    dayOfStudy()
     userCredL = input('Enter your MIREA login >> ')
     userCredP = getpass.getpass('Enter your MIREA password >> ')
 
