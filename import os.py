f5ae25deeb9300cf79fc1f849e14142a53688426
AlexRetr012 <printhelloworld@mail.ru>
2022-02-21 20:02:17 +0300

    0.0.1(First things first)

---
+++ import os.py
@@ -0,0 +1,19 @@
+import os
+
+def task():
+    os.system('clear' if os.name == 'posix' else 'cls')
+
+    print('Welcome to AutoLogginer Lite.\nWarning!We don`t collect/sell/store your logins & passwords \nVer 0.0.1(before release)')
+    print('Enter your MIREA login >> ')
+    
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    task()
